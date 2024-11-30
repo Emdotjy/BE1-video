9660484c0dd6d6e9d0e032850e1fda59ba4d1fea
Vincent <simon.vincent@ecl21.ec-lyon.fr>
2024-11-30 16:41:42 +0100

    ajout de la base de donnée, reste à faire comparaison

--- reconnaissance_sequence.py
+++ reconnaissance_sequence.py
@@ -1,6 +1,8 @@
 import cv2
 from obtain_array import *
 import numpy as np
+import os
+import json
 
 from segmentation_video import *
 def is_black(frame):
@@ -27,8 +29,11 @@ def segmentation_spot_pub(video):
     #on de même, on enlève le début
     debuts.pop(0)
     fins.pop(0)
+    pub_list = [video[debuts[i]:fins[i]] for i in range(len(debuts))]    
+    for num_pub, pub in enumerate(pub_list):
+        print(f"Sequence pub n°{num_pub+1}, démarre à {debuts[num_pub]} et se termine à {fins[num_pub]}")
 
-    return debuts,fins
+    return pub_list
 
 def detection_transitions_from_pub(video):
     similarite_couleur = calculer_similarite_couleur(video)
@@ -64,20 +69,66 @@ def get_rpz_images(pub,silent):
     return images_rpz
 
 
+def add_pub_to_bdd(pub):
+
+    silent = True
+    rpz_images = get_rpz_images(pub, silent)
+    #---------Enregistrement dans la base de donnée --------------------------------------------------------
+
+    # Vérifier et créer le dossier si nécessaire
+    bdd_path = 'bdd'
+    if not os.path.exists(bdd_path):       
+        os.makedirs(bdd_path)
+
+    folders = [f for f in os.listdir(bdd_path) if os.path.isdir(os.path.join(bdd_path, f))]
+    num_pub = len(folders)
+
+    num_pub, pub
+        # Vérifier et créer le dossier spécifique pour la pub, identifié avec son numéro
+    pub_path = bdd_path+f'/images_representatives_pub_n{str(num_pub+1)}'
+    if not os.path.exists(pub_path):       
+        os.makedirs(pub_path)            
+        #enregistrer les images et le deltat
+    for num_image,image in enumerate(rpz_images):
+
+        cv2.imwrite(pub_path+f'/sequence n{num_image+1}.png',image)
+
+    metadata_path = os.path.join(pub_path, "metadata.json")
+    metadata = {}
+    #si metadata existe déja, on le modifie
+    if os.path.exists(metadata_path):
+        with open(metadata_path, 'r') as file:
+            metadata = json.load(file)
+            
+    metadata['pub_length'] = len(pub)
+            
+    with open(metadata_path, 'w') as file:
+        json.dump(metadata, file, indent=4)
+    print(f"Metadata saved: {metadata_path}")
+
+
+
+def recognise_pub_in_bdd(video_path):
+    pass
+
+
 if __name__ == "__main__":
     video_path = 'Pub_C+_352_288_1.mp4'  # Chemin de la vidéo
     video = convertir_video_en_array(video_path)
-    debuts, fins = segmentation_spot_pub(video)
 
-    pub_list = [video[debuts[i]:fins[i]] for i in range(len(debuts))]    
+    pub_list = segmentation_spot_pub(video)
+    
 
-    pub_rpz_images_dict = {}
+    
+    
+    for pub in pub_list:
+        add_pub_to_bdd(pub)
+    video_test_path = 'Pub_C+_352_288_2.mp4'    
+    video = convertir_video_en_array(video_path)
+    pub_list = segmentation_spot_pub(video)
 
-    silent = True
-    for num_pub, pub in enumerate(pub_list):
-        print(f"Sequence pub n°{num_pub}, démarre à {debuts[num_pub]} et se termine à {fins[num_pub]}")
-        rpz_images = get_rpz_images(pub, silent)
-        pub_rpz_images_dict[f"pub n°{num_pub} from the file {video_path}"] = rpz_images
+
+
 
 
 
